be899b98714bebdfcea36b771eded9fc046fef46
Rajmohan Banavi <rajmohanbanavi@gmail.com>
2016-03-07 17:54:16 +0530

    Added support for proxying Websocket connections as well from HTTPS-to-HTTP

--- app.js
+++ app.js
@@ -51,3 +51,44 @@ webProxy.on('error', function (e) {
 });
 
 console.log('https web server proxy '.blue + 'started '.green.bold + 'on port '.blue + '8080 '.yellow);
+
+
+// notification server websocket proxy
+var wsProxy = httpProxy.createServer({
+    target: {
+        host: '10.10.30.240',
+        port: 3030
+    },
+    ssl: {
+        key: fs.readFileSync('ia.key', 'utf8'),
+        cert: fs.readFileSync('ia.crt', 'utf8')
+    },
+    ws: true
+}).listen(3030);
+
+wsProxy.on('error', function (e) {
+    console.log("WS Proxy => Some error happened: " + JSON.stringify(e));
+});
+
+wsProxy.on('upgrade', function (req, socket, head) {
+    console.log("Upgrading http to websocket");
+    console.log("REQ: " + JSON.stringify(req));
+    console.log("HEAD: " + JSON.stringify(head));
+    proxy.ws(req, socket, head);
+});
+
+wsProxy.on('proxyReqWs', function(proxyReq, req, socket, options, head) {
+    proxyReq.setHeader('Host', proxyReq.getHeader('host'));
+    proxyReq.setHeader('Upgrade', proxyReq.getHeader('upgrade'));
+    proxyReq.setHeader('Connection', proxyReq.getHeader('connection'));
+    proxyReq.setHeader('Origin', proxyReq.getHeader('origin'));
+
+    proxyReq.setHeader('Sec-WebSocket-Version', proxyReq.getHeader('sec-websocket-version'));
+    proxyReq.setHeader('Sec-WebSocket-Key', proxyReq.getHeader('sec-websocket-key'));
+    proxyReq.setHeader('Sec-WebSocket-Extensions', proxyReq.getHeader('sec-websocket-extensions'));
+
+    proxyReq.setHeader('Accept-Encoding', proxyReq.getHeader('accept-encoding'));
+    proxyReq.setHeader('Accept-Language', proxyReq.getHeader('accept-language'));
+});
+
+console.log('WS web server proxy '.red.bold + 'started '.green.bold + 'on port '.yellow + '8080 '.green);
